4c2fb2e9004a40d21e1fcc503ab9682b1d5cc29f
steve <steve@rapidream.com>
2024-03-27 15:16:22 +0300

    FEAT: First dev commit

---
+++ assets/milkyway.tsx
@@ -0,0 +1,22 @@
+import Image from 'next/image';
+import milkyway from '../public/bg.jpg';
+
+export default function Background() {
+  return (
+    <Image
+      alt="Mountains"
+      src={milkyway}
+      placeholder="blur"
+      quality={100}
+      fill
+      sizes=""
+      className="bg"
+      style={{
+        width: '100%',
+        height: '100%',
+        objectFit: 'cover',
+        backgroundPosition: 'center',
+      }}
+    />
+  );
+}
