d66ede10e9a1ba88d7a58f912d2c004847c222e2
steve <steve@rapidream.com>
2024-04-03 14:45:33 +0300

    FEAT: dev changes

---
+++ data/data.ts
@@ -0,0 +1,74 @@
+const FrontEnd = [
+  'JavaScript',
+  'TypeScript',
+  'ES6',
+  'React JS',
+  'Next JS',
+  'Redux',
+  'React Native',
+  'Bootstrap',
+  'Tailwind CSS',
+  'Material UI',
+  'Semantic UI',
+  'WordPress + CMS Development',
+  'Semantic Ui',
+];
+
+const BackEnd = [
+  'Node JS',
+  'Express JS',
+  'Nest JS',
+  'Strapi',
+  'GraphQL',
+  'Prisma',
+  'Koa JS',
+  'PostgreSQL',
+  'API & Microservices development',
+  'Knowledge of back-end development best practices',
+];
+
+const SeversnDevOps = [
+  'Docker',
+  'Nginx',
+  'AWS',
+  'Google Cloud',
+  'Firewalls',
+  'CI/CD',
+];
+
+const Database = ['MongoDB', 'PostgreSQL', 'MySQL', 'SQLite', 'Firebase'];
+
+const scodecontrol = ['Git', 'GitHub', 'GitLab', 'Bitbucket'];
+
+const thirdPartyAPI = [
+  'Chat gpt API',
+  'Google Maps API',
+  'WhatsApp Business API',
+];
+
+export const categories = [
+  {
+    name: 'Front End',
+    technologies: FrontEnd,
+  },
+  {
+    name: 'Back End',
+    technologies: BackEnd,
+  },
+  {
+    name: 'Servers & DevOps',
+    technologies: SeversnDevOps,
+  },
+  {
+    name: 'Database',
+    technologies: Database,
+  },
+  {
+    name: 'Source Control',
+    technologies: scodecontrol,
+  },
+  {
+    name: 'Third Party APIs',
+    technologies: thirdPartyAPI,
+  },
+];
