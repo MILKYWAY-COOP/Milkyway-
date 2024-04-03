c566a6d5abda534589994fedee34ddf9dcad8745
steve <steve@rapidream.com>
2024-04-03 14:45:05 +0300

    FEAT: dev changes

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -11,6 +11,13 @@ const config: Config = {
       backgroundImage: {},
       colors: {
         white: '#ffffff',
+        darkBlue: 'rgba(4, 3, 57, 0.7)',
+        fadedDarkBlue: 'rgba(4, 3, 57, 0.9)',
+        turquoise: 'rgb(0, 255, 255)',
+        grey: 'rgba(255, 255, 255, 0.7)',
+      },
+      fontFamily: {
+        comfota: ['Poppins', 'sans-serif'],
       },
     },
   },
