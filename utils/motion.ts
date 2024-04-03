9f8af009e161e1c944e8a1247db43b3eba94ab2a
steve <steve@rapidream.com>
2024-04-03 14:46:09 +0300

    FEAT: dev changes

---
+++ utils/motion.ts
@@ -0,0 +1,69 @@
+export function slideInFromLeft(delay: number) {
+  return {
+    hidden: { x: -100, opacity: 0 },
+    visible: {
+      x: 0,
+      opacity: 1,
+      transition: {
+        delay: delay,
+        duration: 0.5,
+      },
+    },
+  };
+}
+
+export function slideInFromRight(delay: number) {
+  return {
+    hidden: { x: 100, opacity: 0 },
+    visible: {
+      x: 0,
+      opacity: 1,
+      transition: {
+        delay: delay,
+        duration: 0.5,
+      },
+    },
+  };
+}
+
+export const slideInFromTop = {
+  hidden: { y: -100, opacity: 0 },
+  visible: {
+    y: 0,
+    opacity: 1,
+    transition: {
+      delay: 0.5,
+      duration: 0.5,
+    },
+  },
+};
+
+export const slideBracketRight = {
+  hidden: {
+    x: 0,
+    opacity: 0,
+  },
+  hover: {
+    x: 5,
+    opacity: 1,
+    transition: {
+      x: { duration: 0.5, ease: 'easeOut' },
+      opacity: { duration: 0.2 },
+    },
+  },
+};
+
+export const slideBracketLeft = {
+  hidden: {
+    x: 0,
+    opacity: 0,
+  },
+  hover: {
+    x: -10,
+    opacity: 1,
+    transition: {
+      x: { duration: 0.5, ease: 'easeOut' },
+      opacity: { duration: 0.2 },
+    },
+  },
+};
