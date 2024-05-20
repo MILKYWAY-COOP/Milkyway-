90176b0b6eeb25ea0dcd94d005658bffc1660e70
steve <steve@rapidream.com>
2024-05-20 16:13:34 +0300

    chore: Update index.tsx to adhere to code style conventions

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,19 +1,19 @@
-import { Application } from '@/context';
+import { Application } from "@/context";
 
-import Hero from '@/components/Hero';
-import Cards from '@/components/Cards';
-import About from '@/components/About';
-import Giving from '@/components/Giving';
-import Projects from '@/components/Projects';
-import Contact from '@/components/Contact';
-import Footer from '@/components/Footer';
-import './globals.css';
+import Hero from "@/components/Hero";
+import Cards from "@/components/Cards";
+import About from "@/components/About";
+import Giving from "@/components/Giving";
+import Projects from "@/components/Projects";
+import Contact from "@/components/Contact";
+import Footer from "@/components/Footer";
+import "./globals.css";
 
 export default function Page() {
   return (
     <Application>
       <div
-        className={`w-full flex flex-col items-start justify-center relative bg-stars gap-[60px] lg:gap-[160px]  app`}
+        className={`w-full flex flex-col items-start justify-center relative bg-stars gap-[60px] lg:gap-[160px] app`}
       >
         <Hero />
         <div className="flex flex-col md:gap-[80px] gap-[60px] lg:gap-[100px] px-[20px] md:px-[104px] xl:px-[150px]">
