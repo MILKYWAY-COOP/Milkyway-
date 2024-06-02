2fd89a38a3ffae77f44c2f2dc31c30fc608ca845
steve <steve@rapidream.com>
2024-06-03 00:19:26 +0300

    chore: Update landing page components for improved styling and responsiveness

--- pages/index.tsx
+++ pages/index.tsx
@@ -17,17 +17,17 @@ export default function Page() {
           className={`w-full flex flex-col items-start justify-center relative  gap-[60px] lg:gap-[160px] app`}
         >
           <Hero />
+          <div className="sm:w-[640px] md:w-[768px] lg:w-[1280px] flex flex-col items-center md:gap-[80px] gap-[60px] lg:gap-[100px] px-[20px] self-center">
+            <Cards />
+            <About />
+          </div>
+          <Giving />
           <div
             className="sm:w-[640px] md:w-[768px] lg:w-[1280px] flex flex-col justify-center xl:gap-[80px] gap-[60px] lg:gap-[100px] px-[20px] self-center"
             id="Work"
           >
             <Projects />
           </div>
-          <div className="sm:w-[640px] md:w-[768px] lg:w-[1280px] flex flex-col items-center md:gap-[80px] gap-[60px] lg:gap-[100px] px-[20px] self-center">
-            <Cards />
-            <About />
-          </div>
-          <Giving />
           <div className="sm:w-[640px] md:w-[768px] lg:w-[1280px] flex flex-col xl:gap-[80px] gap-[60px] lg:gap-[100px] px-[20px] self-center">
             <Contact />
           </div>
